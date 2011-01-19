a8b7d67e7793b1ed2a9c4f96b4dd48de54b4144c
Linus Lundahl <lundahl.linus@gmail.com>
2011-01-20 00:38:35 +0100

    I am the author!

--- good-old-gallery.php
+++ good-old-gallery.php
@@ -5,7 +5,7 @@
  * Plugin Name: Good Old Gallery
  * Plugin URI: http://unwi.se/good-old-gallery
  * Description: Good Old Gallery is a WordPress plugin that helps you upload image galleries that can be used on more than one page/post, it utilizes the built in gallery functionality in WP. Other features include built in jQuery Cycle support and Widgets.
- * Author: Good Old
+ * Author: Linus Lundahl
  * Version: 0.1-dev
  * Author URI: http://unwi.se/
  *
@@ -15,6 +15,12 @@
 define('GOG_PLUGIN_URL', WP_PLUGIN_URL . '/' . plugin_basename( dirname(__FILE__) ));
 define('GOG_PLUGIN_NAME', 'Good Old Gallery');
 define('GOG_PLUGIN_SHORT', 'gog');
+
+// Just tag the page for fun
+function goodold_gallery_add_head_tag() {
+	echo "\n" . '<!-- This site uses Good Old Gallery, get it from http://unwi.se/good-old-gallery -->' . "\n\n";
+}
+add_action('wp_head', 'goodold_gallery_add_head_tag');
 
 // Register style and js for this plugin
 if ( !is_admin() ) {
--- readme.txt
+++ readme.txt
@@ -1,8 +1,8 @@
 === Good Old Gallery ===
 Contributors: linuslundahl
 Donate link:
-Tags: simple, image, gallery, slideshow, jQuery
-Requires at least: 3.0.4
+Tags: simple, image, gallery, slideshow, jQuery, multiple
+Requires at least: 3.0
 Tested up to: 3.0.4
 Stable tag: 0.1-dev
 
@@ -18,7 +18,7 @@ Included is jQuery Cycle.
 
 1. Upload `good-old-gallery` to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
-3. Go to 'Gallery -> Settings' to setup the basic settings.
+3. Go to `Gallery -> Settings` to setup the basic settings.
 4. Upload images on a gallery post, go to a post or page in click the new gallery media icon to insert your gallery.
 
 == Changelog ==
